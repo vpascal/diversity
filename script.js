ff35c4571a4b912b0e25afc3f03147f0031e0840
vpascal <vpascal2@gmail.com>
2018-09-25 16:06:48 -0400

    js-dependecies

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+    $("#patton").percircle({
+      progressBarColor: "#CC3366",
+      percent: 18.5
+    });
+});
+
+
+$(function(){
+  $("#ou").percircle({
+      percent: 15.3
+    });
+});
