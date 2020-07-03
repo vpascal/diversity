73b4d1195ead46bd0c186821bba3f42946c0afa1
vpascal <vpascal2@gmail.com>
2020-07-03 14:23:26 -0400

    data fix

--- update.js
+++ update.js
@@ -1,13 +1,9 @@
 
-import * as d3 from "d3";
-
-d3.csv("/data/line _chart.csv", function(d) {
+d3.csv("data/linechart.csv", function(error,d) {
   return {
     year : d.year,
     category : d.category,
     type : d.type,
     value : +d.value
   };
-}).then(function(data) {
-  console.log(data[0]);
 });
